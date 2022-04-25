8f04aa7cd623f8e7fea684caf03a304fe99d42da
hoang.tran-nguyen <hoang.tran-nguyen@soget.fr>
2022-04-25 15:04:52 +0700

    Edit

--- hello.js
+++ hello.js
@@ -18,10 +18,5 @@ app.get("/", (req, res) => {
     res.send("Hello World, from express");
 });
  
-var server = app.listen(8081, function () {
-    var host = server.address().address
-    var port = server.address().port
-    
-    console.log("Example app listening at http://%s:%s", host, port)
- })
+app.listen(process.env.PORT || 8081)
 
